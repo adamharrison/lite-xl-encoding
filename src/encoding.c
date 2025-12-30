9ce42458ae68f33a76060c6f19762fa38ec19263
Adam Harrison <adamdharrison@gmail.com>
2025-12-29 21:28:42 -0500

    Reworded utf8 character detection so that it works correctly.

--- src/encoding.c
+++ src/encoding.c
@@ -50,44 +50,26 @@ static bom_t bom_list[] = {
  * For this reason, we included a third party MIT function to check if a string
  * is valid utf8 and prefer this result over the one from uchardet.
 */
-/*************************UTF-8 Validation Code********************************/
-/* Found on: https://stackoverflow.com/a/22135005                             */
-/* Copyright (c) 2008-2009 Bjoern Hoehrmann <bjoern@hoehrmann.de>             */
-/* See http://bjoern.hoehrmann.de/utf-8/decoder/dfa/ for details.             */
-#define UTF8_ACCEPT 0
-#define UTF8_REJECT 1
-
-static const unsigned char utf8d[] = {
-  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, // 00..1f
-  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, // 20..3f
-  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, // 40..5f
-  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, // 60..7f
-  1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9, // 80..9f
-  7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7, // a0..bf
-  8,8,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, // c0..df
-  0xa,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x4,0x3,0x3, // e0..ef
-  0xb,0x6,0x6,0x6,0x5,0x8,0x8,0x8,0x8,0x8,0x8,0x8,0x8,0x8,0x8,0x8, // f0..ff
-  0x0,0x1,0x2,0x3,0x5,0x8,0x7,0x1,0x1,0x1,0x4,0x6,0x1,0x1,0x1,0x1, // s0..s0
-  1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,1, // s1..s2
-  1,2,1,1,1,1,1,2,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1, // s3..s4
-  1,2,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,3,1,1,1,1,1,1, // s5..s6
-  1,3,1,1,1,1,1,3,1,3,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1, // s7..s8
-};
-
 int utf8_validate(const char *str, size_t len) {
-  int state = UTF8_ACCEPT;
+  int bytes_left = 0;
   for (size_t i = 0; i < len; i++) {
-        // We don't care about the codepoint, so this is
-        // a simplified version of the decode function.
-        int type = utf8d[(int)str[i]];
-        state = utf8d[256 + (state) * 16 + type];
-        if (state == UTF8_REJECT)
-            return 0;
+    int state = str[i];
+    if (bytes_left) {
+      if ((state & 0xC0) != 0x80)
+        return 0;
+      bytes_left--;
+    } else {
+      switch (state & 0xf0) {
+        case 0xf0 :  bytes_left = 3;  break;
+        case 0xe0 :  bytes_left = 2;  break;
+        case 0xd0 :
+        case 0xc0 :  bytes_left = 1;  break;
+        default   :  break;
+      }
     }
-    return 1;
+  }
+  return 1;
 }
-/*************************End of UTF-8 Validation Code*************************/
-
 
 /* Get the applicable byte order marks for the given charset */
 static const char* encoding_bom_from_charset(const char* charset, size_t* len) {
